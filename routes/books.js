19ca18f6011a2614fa75d638a4bbe70dfb89f39b
Lenovo <akapranov872@gmail.com>
2021-09-03 20:09:20 +0300

    nullif

--- routes/books.js
+++ routes/books.js
@@ -20,7 +20,7 @@ router.get('/', function(req, res, next) {
 //display pl page
 router.get('/pl', function(req, res, next) {
 
-    dbConn.query('select COALESCE (localization.title_translation, books.title)\n' +
+    dbConn.query('select COALESCE (nullif(title_translation,\'\'), books.title)\n' +
         'AS title\n' +
         'from language join localization on language.language_id=localization.language_id\n' +
         'join books on books.book_id=localization.book_id where language = "pl"',function(err,rows)     {
@@ -38,7 +38,7 @@ router.get('/pl', function(req, res, next) {
 //display ru page
 router.get('/ru', function(req, res, next) {
 
-    dbConn.query('select COALESCE (localization.title_translation, books.title)\n' +
+    dbConn.query('select COALESCE (nullif(title_translation,\'\'), books.title)\n' +
         'AS title\n' +
         'from language join localization on language.language_id=localization.language_id\n' +
         'join books on books.book_id=localization.book_id where language = "ru"',function(err,rows)     {
@@ -56,7 +56,7 @@ router.get('/ru', function(req, res, next) {
 //display by page
 router.get('/by', function(req, res, next) {
 
-    dbConn.query('select language.language COALESCE (localization.title_translation, books.title)\n' +
+    dbConn.query('select COALESCE (nullif(title_translation,\'\'), books.title)\n' +
         'AS title\n' +
         'from language join localization on language.language_id=localization.language_id\n' +
         'join books on books.book_id=localization.book_id where language = "by"',function(err,rows)     {
